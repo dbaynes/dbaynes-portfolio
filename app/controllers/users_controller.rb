cca66c04d01c25db5e98647257726edd038530f3
David Baynes <dlbaynes@gmail.com>
2014-02-25 18:03:48 -0800

    Encrypted PW

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation,: :encrypted_password)
   end
   
 end
